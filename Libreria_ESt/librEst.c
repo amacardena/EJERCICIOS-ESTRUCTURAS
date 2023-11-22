c31c6e7614c4c323e7fb450fc748ea5de12bc73b
Adrian <Adrian@DESKTOP-75CH9HE>
2023-11-22 12:02:33 +0100

    EJERCICIOS ESTRUCTURAS
    	Ejercicios básicos para aprender a utilizar estructuras

---
+++ Libreria_ESt/librEst.c
@@ -0,0 +1,8 @@
+/*
+ * librEst.c
+ *
+ *  Created on: 22 nov 2023
+ *      Author: Adrian
+ */
+
+
---
+++ Libreria_ESt/librEstr.h
@@ -0,0 +1,13 @@
+/*
+ * librEstr.h
+ *
+ *  Created on: 22 nov 2023
+ *      Author: Adrian
+ */
+
+#ifndef LIBRESTR_H_
+#define LIBRESTR_H_
+
+
+
+#endif /* LIBRESTR_H_ */
