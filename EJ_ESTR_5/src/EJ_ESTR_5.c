0e7c625543b4570a4f2b174d0f495e0adffd3253
Adrian <Adrian@DESKTOP-75CH9HE>
2023-11-27 13:59:23 +0100

    EJERCICIO 5
    EJERCICIO 5 CORREGIDO

--- EJ_ESTR_5/src/EJ_ESTR_5.c
+++ EJ_ESTR_5/src/EJ_ESTR_5.c
@@ -25,6 +25,7 @@ int main(void) {
 		fflush(stdin);
 		printf("\nIntroduce la persona numero %i de %i. (Pulse solo intro si quiere salir) --> ", i+1, MAXEJ5);
 		fflush(stdout); fgets(lista[i].nombre, MAXNOMBRE,stdin);
+
 		lista[i].nombre[strlen(lista[i].nombre)-1] = '\0';
 
 		if(strlen(lista[i].nombre) > 0)
@@ -45,62 +46,66 @@ int main(void) {
 		else break;
 	} while(i < MAXEJ5);
 
-	do
+	if(i > 0)
 	{
-		fflush(stdin);
-		printf("\n1.- Mostrar todas las personas");
-		printf("\n2.- Mostrar las personas de una cierta edad");
-		printf("\n3.- Mostrar las personas cuya inicial sea la que el usuario indique");
-		printf("\n0.- Salir del programa");
-		printf("\nElige una opcion --> ");
-		fflush(stdout);		scanf("%i", &menu);
-
-		switch(menu)
+		do
 		{
-			case 1:
-				for(j = 0; j < i; j++)
-				{
-					printf("\n\nPersona %i", j+1);
-					mostrarPers5(lista, j);
-				}
-			break;
-			case 2:
-				edadUsuario = -1;
-					do {
-						printf("\nIntroduce la edad de las personas que estas buscando --> ");
-						fflush(stdout);		scanf("%i", &edadUsuario);
-					} while(edadUsuario < 0);
+			fflush(stdin);
+			printf("\n1.- Mostrar todas las personas");
+			printf("\n2.- Mostrar las personas de una cierta edad");
+			printf("\n3.- Mostrar las personas cuya inicial sea la que el usuario indique");
+			printf("\n0.- Salir del programa");
+			printf("\nElige una opcion --> ");
+			fflush(stdout);		scanf("%i", &menu);
 
+			switch(menu)
+			{
+				case 1:
 					for(j = 0; j < i; j++)
 					{
-						if(edadUsuario == lista[j].edad)
-						{
-							printf("\n\nPersona %i", j+1);
-							mostrarPers5(lista, j);
-						}
+						printf("\n\nPersona %i", j+1);
+						mostrarPers5(lista, j);
 					}
-			break;
-			case 3:
-				fflush(stdin);
-					printf("\nIntroduce la inicial de las personas que quieres buscar --> ");
-					fflush(stdout);	inicialUsuario=toupper(getchar());
+				break;
+				case 2:
+					edadUsuario = -1;
+						do {
+							printf("\nIntroduce la edad de las personas que estas buscando --> ");
+							fflush(stdout);		scanf("%i", &edadUsuario);
+						} while(edadUsuario < 0);
+
+						for(j = 0; j < i; j++)
+						{
+							if(edadUsuario == lista[j].edad)
+							{
+								printf("\n\nPersona %i", j+1);
+								mostrarPers5(lista, j);
+							}
+						}
+				break;
+				case 3:
 					fflush(stdin);
-					for(j = 0; j < i; j++)
-					{
-						lista[j].nombre[0] = toupper(lista[j].nombre[0]);
-
-						if(inicialUsuario == lista[j].nombre[0])
+						printf("\nIntroduce la inicial de las personas que quieres buscar --> ");
+						fflush(stdout);	inicialUsuario=toupper(getchar());
+						fflush(stdin);
+						for(j = 0; j < i; j++)
 						{
-							printf("\n\nPersona %i", j+1);
-							mostrarPers5(lista, j);
+							lista[j].nombre[0] = toupper(lista[j].nombre[0]);
+
+							if(inicialUsuario == lista[j].nombre[0])
+							{
+								printf("\n\nPersona %i", j+1);
+								mostrarPers5(lista, j);
+							}
 						}
-					}
-			break;
-			case 0: 	break;
-			break;
-			default: printf("\nOpcion incorrecta. Vuelvela a introducir.\n");
-		}
-	} while (menu != 0);
+				break;
+				case 0: 	break;
+				break;
+				default: printf("\nOpcion incorrecta. Vuelvela a introducir.\n");
+			}
+		} while (menu != 0);
+	}
+
 	printf("\nAdios (:");
 	return EXIT_SUCCESS;
 }
